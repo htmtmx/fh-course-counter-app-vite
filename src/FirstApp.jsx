5d49e5cae4e6ec790232e77de4781f699d61b25e
htmtmx <cesar.pineda@daksha.mx>
2024-06-11 17:44:35 -0600

    🏷️ Add PropTypes for type checking to FirstApp component

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,9 +1,16 @@
-export const FirstApp = ({title, subtitle}) => {
-  console.log(title)
+import PropTypes from 'prop-types';
+
+export const FirstApp = ({ title, subtitle }) => {
+
   return (
     <>
       <h1>{ title }</h1>
       <p>{ subtitle }</p>
     </>
   )
+}
+
+FirstApp.propTypes = {
+  title: PropTypes.string.isRequired,
+  subtitle: PropTypes.string
 }
--- src/main.jsx
+++ src/main.jsx
@@ -15,7 +15,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={darkTheme}>
       <CssBaseline/>
-      <FirstApp title='Titulo pasado por props' subtitle='lorem' />
+      <FirstApp title='Usando PropTypes' subtitle='Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque omnis eius neque. Veritatis, vitae voluptas! Magni ad, soluta dolor optio earum accusamus odio impedit nam, quidem similique nobis, obcaecati id!' />
+      
     </ThemeProvider>
   </React.StrictMode>
 );

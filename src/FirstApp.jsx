33489d0f5d716e118ce9e9ceb4077cd3aabea63a
htmtmx <cesar.pineda@daksha.mx>
2024-06-11 13:10:56 -0600

    🚧 Wrap two html labels in empty fragment

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,5 +1,8 @@
 export const FirstApp = () => {
   return (
+    <>
     <h1>Cesar</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias deserunt hic eveniet eligendi ipsum illum quam itaque accusamus dignissimos voluptatem, similique quas neque, ullam voluptatibus provident reiciendis doloremque esse nesciunt!</p>
+    </>
   )
 }

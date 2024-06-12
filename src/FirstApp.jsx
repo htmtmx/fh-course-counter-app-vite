23035aac2befe2f8cab70dfc63ca1bf5198ccd95
htmtmx <cesar.pineda@daksha.mx>
2024-06-12 11:51:19 -0600

    🚧 Update FirstApp component to use default parameter values

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,16 +1,27 @@
 import PropTypes from 'prop-types';
 
-export const FirstApp = ({ title, subtitle }) => {
+export const FirstApp = ({ name='Cesar Pineda', title='Sin título', subtitle='Sin subtítulo' }) => {
 
   return (
     <>
       <h1>{ title }</h1>
+      <h1>{ name }</h1>
       <p>{ subtitle }</p>
     </>
   )
 }
 
 FirstApp.propTypes = {
+  name: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   subtitle: PropTypes.string
 }
+
+//! defaultProps will be deprecated in the future
+//! It's better to use the default values in the parameters
+
+// FirstApp.defaultProps = {
+//   name: 'Cesar Pineda',
+//   subtitle: 'Sin subtitulo',
+//   title: 'Sin titulo',
+// }
--- src/main.jsx
+++ src/main.jsx
@@ -15,7 +15,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={darkTheme}>
       <CssBaseline/>
-      <FirstApp title='Usando PropTypes' subtitle='Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque omnis eius neque. Veritatis, vitae voluptas! Magni ad, soluta dolor optio earum accusamus odio impedit nam, quidem similique nobis, obcaecati id!' />
+      <FirstApp name='Haziel Pineda' title='Valores default en parametros' />
       
     </ThemeProvider>
   </React.StrictMode>

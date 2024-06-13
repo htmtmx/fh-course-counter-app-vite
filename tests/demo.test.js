15c8cbc7b76ef2af91dadc96eca50f3ed15116b0
htmtmx <cesar.pineda@daksha.mx>
2024-06-13 14:21:52 -0600

    ✅ Add test for trimming whitespace in MyComponent

--- tests/demo.test.js
+++ tests/demo.test.js
@@ -1,4 +1,16 @@
-test('Esta prueba no debe fallar', () => { 
-  if (1 === 0) {
-    throw new Error('No es igual a 0');  }
- })
+
+describe('Pruebas en <MyComponent/>', () => {
+  test('Esta prueba no debe fallar', () => { 
+    
+    //! inicializacion
+    const message1 = 'Hola mundo         ';
+    //! estimulo
+    const message2 = message1.trim();
+  
+    //! Observar el comportamiento... esperado
+    expect(message1).toBe(message2);
+  })
+})
+
+// expect en Jest es una función que recibe un valor y retorna un objeto con funciones que permiten evaluar el valor recibido.
+// toBe es una función que recibe un valor y evalúa si es igual al valor recibido en expect.

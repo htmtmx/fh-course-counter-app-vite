28c78674085ce7703ba2a5b2cb955e0daf2ae003
htmtmx <cesar.pineda@daksha.mx>
2024-06-17 13:20:12 -0600

    ✅ Add tests for getUser and getUsuarioActivo functions in 05-funciones.test.js

---
+++ tests/base-pruebas/05-funciones.test.js
@@ -0,0 +1,20 @@
+import { getUser, getUsuarioActivo } from '../../src/base-pruebas/05-funciones';
+describe('Pruebas en 05-funciones', () => {
+  test('getUser debe retornar un objeto', () => {
+    const testUser = {
+      uid: 'ABC123',
+      username: 'El_Papi1502',
+    }
+    const user = getUser();
+    expect(testUser).toStrictEqual(user);
+  });
+
+  test('getUsuario debe retornar un objeto', () => { 
+    const testUser = {
+      uid: 'ABC567',
+      username: 'Cesar'
+    };
+    const user = getUsuarioActivo(testUser.username);
+    expect(testUser).toStrictEqual(user);
+   })
+});

aba8e4e1a7179fea481038f5959ec51994c26223
htmtmx <cesar.pineda@daksha.mx>
2024-06-18 17:49:27 -0600

    ✅ Update FirstApp component test to handle new title styling updates

--- tests/FirstApp.test.jsx
+++ tests/FirstApp.test.jsx
@@ -6,9 +6,23 @@ describe('Pruebas en FirstApp', () => {
   // eslint-disable-next-line no-undef
   test('Debe hacer match con el snapshot', () => {
     
-    const title = 'Sola soy Cesar';
-    const name = 'Cesar'
-    render(<FirstApp name={name} title={title} />);
-
+    const title = 'Presentación:';
+    const name = 'Hola soy Cesar'
+    const { container } = render(<FirstApp name={name} title={title} />);
+    // eslint-disable-next-line no-undef
+    expect(container).toMatchSnapshot();
   });
+  // eslint-disable-next-line no-undef
+  test('Debe de mostrar el titulo en un h1', () => { 
+    const title = 'Presentación:';
+    const name = 'Hola soy Cesar'
+    const { container, getByText } = render(<FirstApp name={name} title={title} />);
+    // eslint-disable-next-line no-undef
+    expect(getByText(title)).toBeTruthy();
+    
+    const h1 = container.querySelector('h1');
+    // eslint-disable-next-line no-undef
+    expect(h1.innerHTML).toContain(title);
+    // expect(h1)
+   })
 });

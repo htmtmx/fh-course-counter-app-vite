c0463dac78526b7008d493bd293477d5ba81c421
htmtmx <cesar.pineda@daksha.mx>
2024-06-18 15:51:20 -0600

    ♻️ Refactor getImagen function in 11-async-await.js

--- tests/base-pruebas/11-async-await.test.js
+++ tests/base-pruebas/11-async-await.test.js
@@ -2,9 +2,9 @@ import { getImagen } from "../../src/base-pruebas/11-async-await";
 
 describe('Pruebas 11-async-await', () => {
   
-  test('getImagen debe retornar una url de la imagen', async() => {
-    const url = await getImagen();
-    expect(typeof url).toBe('string');
+  test('getImagen debe retornar un error sino tenemos api key', async() => {
+    const resp = await getImagen();
+    expect(resp).toBe('No se encontro la imagen');
   });
 
 });
